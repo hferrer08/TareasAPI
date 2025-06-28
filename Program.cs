699930857499e8abf81567cee528aa5549a54a01
hferrer08 <hubert.ferrerg8@gmail.com>
2025-06-27 22:05:38 -0400

    Se corrige CORS para seguridad habilitada

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using TareasAPI.Data;
- using System.IO;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
-//Conexión base de datos
+
+// Conexión base de datos
 var baseDir = AppDomain.CurrentDomain.BaseDirectory;
 var dbPath = Path.Combine(baseDir, "Tareas.db");
 var connectionString = $"Data Source={dbPath}";
@@ -11,19 +12,7 @@ var connectionString = $"Data Source={dbPath}";
 builder.Services.AddDbContext<TareasContext>(options =>
     options.UseSqlite(connectionString));
 
-// Agregar servicios de controllers y endpoints
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
-
+// Configurar CORS antes de construir la app
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -32,7 +21,18 @@ builder.Services.AddCors(options =>
                           .AllowAnyHeader());
 });
 
+// Servicios
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middlewares en orden correcto
 app.UseCors("AllowAll");
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
